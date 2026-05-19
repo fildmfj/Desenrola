fd57e731f583eeacdf22aad06ce1dbe0ff8b28be
Phelps <fillipenonatofetter@gmail.com>
2026-05-19 08:01:59 -0300

    Conexão com o MySQL e servidor em python

--- Codigo/Site/python.py
+++ Codigo/Site/python.py
@@ -1,3 +1,18 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="localhost",
+    database="Desenrola",
+    user="root",
+    password="root"
+)
+
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM usuarios")
+rows = cursor.fetchall()
+
+conn.close()
+
 from flask import Flask, render_template
 
 
@@ -5,7 +20,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def principal():
-    return render_template('Cadastro.html')
+    return render_template('Alunos.html')
 
 if __name__ == '__main__':
     app.run(debug=True) 
